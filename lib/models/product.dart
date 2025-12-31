a4c37d1bcb3943cc01145704395b4b75b34f0231
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2025-12-31 14:21:16 +0330

    Update product.dart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,12 +3,14 @@ class Product {
   final String price;
   final String image;
   final String description;
+  final String category; // فیلد جدید
 
   Product({
     required this.title, 
     required this.price, 
     required this.image, 
-    required this.description
+    required this.description,
+    required this.category,
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
@@ -17,6 +19,7 @@ class Product {
       price: json['price']?.toString() ?? '0',
       image: json['image'] ?? '',
       description: json['description'] ?? '',
+      category: json['category'] ?? 'عمومی', // مقدار پیش‌فرض
     );
   }
 }
